1d79633fec52bb4f61abcd88600f2b19adc2d5e2
Sean <seankim912@yahoo.com>
2023-10-25 18:00:59 -0500

    refactored with terminal-in-react package instead

--- my-new-app/ClientApp/src/components/TerminalController.js
+++ my-new-app/ClientApp/src/components/TerminalController.js
@@ -1,22 +1,50 @@
-import React, { useState } from 'react';
-import Terminal, { ColorMode, TerminalOutput } from 'react-terminal-ui';
+import React from 'react';
+import Terminal from 'terminal-in-react';
 
 const TerminalController = () => {
-    const [terminalLineData, setTerminalLineData] = useState([
-        <TerminalOutput>Welcome to the React Terminal UI Demo!</TerminalOutput>
-    ]);
+    // let showMsg = () => "Hey Hey Hey";
 
     return (
         <>
             <Terminal
-                name='React Terminal Usage Example'
-                colorMode={ ColorMode.Dark }
-                onInput={ terminalInput => console.log(`New terminal input received: '${ terminalInput }'`)}
-            >
-                { terminalLineData }
-            </Terminal>
+                color='green'
+                backgroundColor='black'
+                barColor='black'
+                style={{ fontWeight: "bold", fontSize: "1em" }}
+                commands={{
+                    'open-google': () => window.open('google.com', '_blank'),
+                    // showmsg: this.showMsg,
+                    popup: () => alert('Terminal in React')
+                }}
+                descriptions={{
+                    'open-google': 'opens google.com',
+                    // showmsg: 'shows a message',
+                    alert: 'alert', popup: 'alert'
+                }}
+                msg='You can write anything here. Example - Hello! My name is Foo and I like Bar.'
+            />
         </>
     );
 }
 
 export default TerminalController;
+
+// import Terminal, { ColorMode, TerminalOutput } from 'react-terminal-ui';
+
+// const TerminalController = () => {
+//     const [terminalLineData, setTerminalLineData] = useState([
+//         <TerminalOutput>Welcome to the React Terminal UI Demo!</TerminalOutput>
+//     ]);
+
+//     return (
+//         <>
+//             <Terminal
+//                 name='React Terminal Usage Example'
+//                 colorMode={ ColorMode.Dark }
+//                 onInput={ terminalInput => console.log(`New terminal input received: '${ terminalInput }'`)}
+//             >
+//                 { terminalLineData }
+//             </Terminal>
+//         </>
+//     );
+// }
