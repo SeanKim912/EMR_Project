bb2ff655b636e6f2c2a1cb2487120c46ac313737
Sean <seankim912@yahoo.com>
2023-11-10 22:30:02 -0600

    switch to mysql

--- my-new-app/Helpers/DataContext.cs
+++ my-new-app/Helpers/DataContext.cs
@@ -20,3 +20,14 @@ public class DataContext : DbContext
 
     public DbSet<User> Users { get; set; }
 }
+
+public class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "server=localhost; user=root1;password=1234;database=test100";
+
+        var serverVersion = new MySqlServerVersion(new Version(10, 4, 27));
+        optionsBuilder,.UseMySql(connetionString, serverVersion);
+    }
+}
