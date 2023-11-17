8c8496dcdb7f5b45868259209194129a11fa6ca5
Sean <seankim912@yahoo.com>
2023-11-16 21:54:12 -0600

    added first models

--- my-new-app/Data/DataContext.cs
+++ my-new-app/Data/DataContext.cs
@@ -1,9 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using my_new_app.Model;
 
 namespace my_new_app.Data
 {
     public class AppDbContext : DbContext
     {
+
+        public DbSet<Patient> Patient { get; set;}
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             var connectionString = "server=localhost; user=root1;password=1234;database=test100";
---
+++ my-new-app/Model/Patient.cs
@@ -0,0 +1,9 @@
+namespace my_new_app.Model
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
