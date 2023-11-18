c9ed70c41bfccd682663226f5905412ade632caa
Sean <seankim912@yahoo.com>
2023-11-17 23:48:22 -0600

    relational db models expanded upon

--- my-new-app/Model/Provider.cs
+++ my-new-app/Model/Provider.cs
@@ -1,10 +1,11 @@
 namespace my_new_app.Model
 {
-    public class Patient
+    public class Provider
     {
         public int Id { get; set; }
-
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Title { get; set; }
+        public string Specialty { get; set; }
     }
 }
